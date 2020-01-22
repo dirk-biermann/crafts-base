8b96436293c96e2e9e9224ddf1196844751bfb9c
Biermann, Dirk <biermannd@mediamarktsaturn.com>
2020-01-22 10:20:02 +0100

    routing component view-edit back to project overview

--- routes/secret/component-detail.js
+++ routes/secret/component-detail.js
@@ -17,6 +17,45 @@ createComponentController.use((req, res, next) => {
     }                             
 });
 
+createComponentController.get('/:type/:id/:pid', (req, res, next) => {
+    const type = req.params.type;
+    const id = req.params.id;
+    const pid = req.params.pid;
+    console.log( "CD-PID", pid );
+    data.source = `/secret/component/detail/${type}/${id}/${pid}`;
+    if(type == "pattern") {
+        Pattern.findById(id)
+            .then((pattern) => { 
+                pattern.type_prj = '-from-project';
+                pattern.prjId = pid.toString();
+                data.pattern = pattern;
+                console.log(data);
+                res.render('secret/component-detail-pattern.hbs', data );
+            })
+            .catch(err => {
+                next(err);
+            })
+    };
+    if(type == "fabric") {
+        Fabric.findById(id)
+            .then((fabric) => {
+                fabric.type_prj = '-from-project';
+                fabric.prjId = pid.toString();
+                data.fabric = fabric;
+                if(fabric.pattern === true){
+                    data.fabric.hasPattern = "checked";
+                }else{
+                    data.fabric.noPattern = "checked";
+                }
+                console.log("Inside fabric edit " , data)
+                res.render('secret/component-detail-fabric.hbs', data )
+            })
+            .catch(err => {
+                next(err);
+            })
+    }; 
+});
+
 createComponentController.get('/:type/:id', (req, res, next) => {
     const type = req.params.type;
     const id = req.params.id;
--- routes/secret/component-edit.js
+++ routes/secret/component-edit.js
@@ -17,6 +17,79 @@ createComponentController.use((req, res, next) => {
     }                             
 });    
 
+createComponentController.get('/:type/:id/:pid', (req, res, next) => {
+    const type = req.params.type;
+    const id = req.params.id;
+    const pid = req.params.id;
+    data.source = `/secret/component/edit/${type}/${id}/${pid}`;
+    
+
+    if(type == "pattern") {
+        Pattern.findById(id)
+            .then((pattern) => { 
+                data.pattern = pattern;
+                pattern.type_prj = '-from-project';
+                pattern.prjId = pid.toString();
+                console.log(data);
+                res.render('secret/component-edit-pattern.hbs', data );
+            })
+            .catch(err => {
+                next(err);
+            })
+    };
+
+    if(type == "fabric") {
+        Fabric.findById(id)
+            .then((fabric) => {
+                data.fabric = fabric;
+                fabric.type_prj = '-from-project';
+                fabric.prjId = pid.toString();
+                if(fabric.pattern === true){
+                    data.fabric.hasPattern = "checked";
+                }else{
+                    data.fabric.noPattern = "checked";
+                }
+                console.log("Inside fabric edit " , data)
+                res.render('secret/component-edit-fabric.hbs', data )
+            })
+            .catch(err => {
+                next(err);
+            })
+    } 
+});
+
+createComponentController.post('/:type/:id/:pid', (req, res, next) => {
+    const type = req.params.type;
+    const id = req.params.id;
+    const pid = req.params.pid;
+    data.source = `/secret/component/edit/${type}/${id}/${pid}`;
+    
+    console.log("POST-EDIT-COMP", type, id, pid);
+
+    if(type == "pattern") {
+        const {name, description, typeOfClothes, instructions, imageUrl} = req.body;
+        Pattern.findByIdAndUpdate(id, {name, description, typeOfClothes, instructions, imageUrl})
+            .then(() => { 
+                res.redirect(`/secret/project/view/${pid}`);
+            })
+            .catch(err => {
+                next(err);
+            })
+    };
+
+    if(type == "fabric") {
+        const {name, description, length, width, imageUrl, material, color, pattern} = req.body;
+        Fabric.findByIdAndUpdate(id, {name, description, length, width, imageUrl, material, color, pattern})
+            .then(() => {
+                res.redirect(`/secret/project/view/${pid}`);
+            })
+            .catch(err => {
+                next(err);
+            })
+    }
+    
+});
+
 createComponentController.get('/:type/:id', (req, res, next) => {
     const type = req.params.type;
     const id = req.params.id;
